2bba8dbdf8a8e212efbb2a091a964186b118bd41
tkassila <tuomas.kassila@gmail.com>
2022-01-29 21:03:23 +0200

    tested run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
-# java -version
-# set ESPEAK_HOME="."
-# SET PATH=%ESPEAK_HOME%;%PATH%
-# echo %ESPEAK_HOME%
-export DISPLAY=:0
-echo java -cp .:./JavaFxmlFileI18Convert.jar;$CLASSPATH com.metait.javafxmlfileI18convert.JavaFxmlFileConver18Application
-java -cp .:./JavaFxmlFileI18Convert.jar:$CLASSPATH com.metait.javafxmlfileI18convert.JavaFxmlFileConver18Application
-# com.metait.javafxplayer.testapp.HelloWorld
+# !#/bin/bash
+#java --version
+# export JAVA_HOME=/home/tk/.sdkman/candidates/java/11.0.13.fx-zulu
+¤ export PATH=$JAVA_HOME/bin:$PATH
+dt_exec_path2=$(pwd)
+
+echo ajohakemisto: $dt_exec_path2
+# ls -la $dt_exec_path2/JavaFxmlFileI18Convert.jar
+echo java -cp $dt_exec_path2/JavaFxmlFileI18Convert.jar:$CLASSPATH:. com.metait.javafxmlfileI18convert.JavaFxmlFileConver18Application
+java -cp $dt_exec_path2/JavaFxmlFileI18Convert.jar:$CLASSPATH:. com.metait.javafxmlfileI18convert.JavaFxmlFileConver18Application

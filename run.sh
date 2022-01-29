400aa4c23b8f88b11fb9ebe15f475cb0cf18b87d
tkassila <tuomas.kassila@gmail.com>
2022-01-29 21:04:26 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-# !#/bin/bash
+!#/bin/bash
 #java --version
 # export JAVA_HOME=/home/tk/.sdkman/candidates/java/11.0.13.fx-zulu
 ¤ export PATH=$JAVA_HOME/bin:$PATH
 dt_exec_path2=$(pwd)
 
-echo ajohakemisto: $dt_exec_path2
+# echo ajohakemisto: $dt_exec_path2
 # ls -la $dt_exec_path2/JavaFxmlFileI18Convert.jar
 echo java -cp $dt_exec_path2/JavaFxmlFileI18Convert.jar:$CLASSPATH:. com.metait.javafxmlfileI18convert.JavaFxmlFileConver18Application
 java -cp $dt_exec_path2/JavaFxmlFileI18Convert.jar:$CLASSPATH:. com.metait.javafxmlfileI18convert.JavaFxmlFileConver18Application
